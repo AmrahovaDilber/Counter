2658fdb5f27189a65353eee974dffcc88129ed3f
AmrahovaDilber <amrahovad911@gmail.com>
2024-07-12 22:43:38 +0400

    added

---
+++ index.js
@@ -0,0 +1,29 @@
+const decreaseBtn = document.querySelector("#decreaseBtn");
+const resetBtn = document.querySelector("#resetBtn");
+const increaseBtn = document.querySelector("#increaseBtn");
+const number = document.querySelector("#number");
+const input = document.querySelector("#input");
+
+const value = parseInt(input.value);
+
+increaseBtn.addEventListener("click", () => {
+  const value = parseInt(input.value);
+  number.innerHTML = parseInt(number.innerHTML) + value;
+  increaseBtn.classList.add("bg-green-400");
+  decreaseBtn.classList.remove("bg-red-500");
+});
+
+decreaseBtn.addEventListener("click", () => {
+  const value = parseInt(input.value);
+  number.innerHTML = parseInt(number.innerHTML) - value;
+  decreaseBtn.classList.add("bg-red-500");
+  increaseBtn.classList.remove("bg-green-400");
+});
+
+resetBtn.addEventListener("click", () => {
+  number.innerHTML = 0;
+  decreaseBtn.classList.remove("bg-red-500");
+  increaseBtn.classList.remove("bg-green-400");
+});
+
+
